28bfadf6341698579ac0e784174d193d22320230
Sephalovable <kayb93@gmail.com>
2017-05-13 22:22:02 -0700

    Add saving of user selection to markings list controller

--- src/app/components/markings-list/markings-list.component.js
+++ src/app/components/markings-list/markings-list.component.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import templateUrl from "./template.html";
 import "./style.scss";
 
@@ -5,7 +7,16 @@ const component = {
     templateUrl, 
     controller: function(DatabaseService){
             this.markings = DatabaseService.getMarkings();
-        }
+            this.selectedMarkings = [];
+            this.selectMarking = function(marking){
+                if (this.selectedMarkings.includes(marking)){
+                    _.pull(this.selectedMarkings, marking);
+                } else {
+                    this.selectedMarkings.push(marking);
+                }
+                                    console.log(this.selectedMarkings);
+            }
+    }
 };
 
 export default component;
