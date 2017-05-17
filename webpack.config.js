90e920bbf7fb7fb91328d664574fbc2d97d2802f
Sephalovable <kayb93@gmail.com>
2017-05-16 19:34:26 -0700

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const htmlEntry = new HtmlPlugin({
-    hash: true,
+    hash: false,
     title: "Nidheim",
     template: path.resolve(__dirname, 'src/index.ejs')
 });
@@ -14,6 +14,7 @@ module.exports = {
         'app': path.join(__dirname, 'src')
     },
     output: {
+        publicPath: '/',
         path: __dirname,
         filename: '[name].js'
     },
@@ -45,7 +46,7 @@ module.exports = {
                 test: /\.(jpg|png|ico|svg|eot|ttf|otf|woff2?)$/,
                 loader: 'file-loader',
                 options: {
-                    name: 'assets/[name]_[hash:7].[ext]'
+                    name: 'assets/[name].[ext]'
                 }
             }
         ]
